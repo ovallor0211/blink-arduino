7017834c5b6696c1a01a6f7db9cfa3422bcbfc5e
ovallor0211 <ovallor0211@g.educaand.es>
2025-03-11 10:06:41 +0100

    buenas

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include <Arduino.h>
+#define LED 13
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(400),
+  delay(100),
   digitalWrite(LED_BUILTIN, LOW);
-  delay(400);
+  delay(1000);
+  Serial.print("Funciona.");
 }
